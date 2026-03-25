fdaad069bea3b5db961c7d1bbf1ab6727413c60f
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2026-03-25 17:32:40 -0300

    Feat: adicionar componente Question consumindo QuizContext e controle de perguntas

---
+++ quiz/src/components/Question.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import Option from "./Option";
+
+import './Question.css';
+
+
+const Question = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+  const currentQuestion = quizState.questions[quizState.currentQuestion];
+
+   return (
+    <div id="question">
+        <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
+        <h2>{currentQuestion.question}</h2>
+        <div id="options-container">
+        </div>
+        <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>Continuar</button>
+    </div>
+   )
+}
+
+export default Question
--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -7,6 +7,7 @@ const STAGES = ["Start", "Playing", "End"];
 const initialState = {
     gameStage: STAGES[0],
     questions,
+    currentQuestion: 0,
 }
 
 const quizReducer = (state, action) => {
@@ -16,6 +17,24 @@ const quizReducer = (state, action) => {
             return {
                 ...state,
                 gameStage: STAGES[1],
+            }
+
+        case "REORDER_QUESTIONS":
+            const reorderQuestion = state.questions.sort(() => {
+                return Math.random() - 0.5;
+            });
+
+            return {
+                ...state,
+                questions: reorderQuestion,
+            };
+        
+        case "CHANGE_QUESTION":
+            const nextQuestion = state.currentQuestion + 1;
+            
+            return{
+                ...state,
+                currentQuestion: nextQuestion,
             }
     }
 }
